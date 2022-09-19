844e5e0cc80c5668d5fce8183c114599cfaeb2e5
Taylor K <taylor.kirk@pm.me>
2022-09-18 21:34:39 -0700

    Create autostart.sh

---
+++ .config/hypr/bin/autostart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+exec-once=swaybg -i $HOME/.config/hypr/wallpapers/pacman.jpeg
+exec-once=foot --server
+exec-once=wlsunset -l -23 -L -46
+exec-once=eww daemon
+exec-once=eww open bar
+exec-once=dunst
