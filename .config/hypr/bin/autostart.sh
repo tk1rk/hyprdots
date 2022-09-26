314f01f98e0bc6c4f7f0824f88d5cc4a2706ab98
Taylor K <taylor.kirk@pm.me>
2022-09-25 23:01:16 -0700

    Update autostart.sh

--- .config/hypr/bin/autostart.sh
+++ .config/hypr/bin/autostart.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
-exec-once=swaybg -i $HOME/.config/hypr/wallpapers/pacman.jpeg
-exec-once=foot --server
-exec-once=wlsunset -l -23 -L -46
+exec-once=swaybg -i $HOME/.config/hypr/wallpapers/wallpaper.png
+exec-once=foot --server nuk nuk or 9o
 exec-once=eww daemon
 exec-once=eww open bar
 exec-once=dunst
+exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+exec-once=killall xdg-desktop-portal xdg-desktop-portal-wlr && /usr/lib/xdg-desktop-portal & /usr/lib/xdg-desktop-portal-wlr
+
